921ca2d93fd5505490eb3e0ae87b778de295dca3
NicoMunoz909 <nicolas.munoz@outlook.es>
2025-02-20 14:49:42 -0500

    Fix typo in import

--- src/Components/ImgTextBelow/index.jsx
+++ src/Components/ImgTextBelow/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./ImgTextBelow.module.css";
+import styles from "./imgTextBelow.module.css";
 import { IoIosArrowDropright } from "react-icons/io";
 
 const ImgTextBelow = ({ pie, link }) => {
